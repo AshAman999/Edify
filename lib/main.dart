b914de3283b921eb815568662e501a156b7cac34
Aman Raj <amanpie9@gmail.com>
2021-07-21 20:14:26 +0530

    up;oad to firebase working

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:edify/screens/addpost.dart';
 import 'package:edify/screens/home.dart';
 import 'package:edify/screens/profile.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
--- lib/screens/addpost.dart
+++ lib/screens/addpost.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+import 'package:edify/database/add_post.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({Key? key}) : super(key: key);
@@ -11,6 +14,9 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  FirebaseHelper helper = FirebaseHelper();
+  bool isloading = false;
+  String imglink = "";
   String title = "", description = "", location = "";
   String imagePath = "";
   final picker = ImagePicker();
@@ -24,11 +30,31 @@ class _AddPostState extends State<AddPost> {
     }
   }
 
-  void postBlog() {
+  void postBlog() async {
     if (pickedFile == null) {
       // add a pop up warning
+
     } else {
-      // upload image to firebase
+      final refr = FirebaseStorage.instance
+          .ref("uploadedImages/${randomAlphaNumeric(8)}.jpg");
+      final task = refr.putFile(File(imagePath));
+      final snapshot = await task.whenComplete(() {
+        setState(() {
+          isloading = false;
+        });
+      });
+      imglink = await snapshot.ref.getDownloadURL();
+      print(imglink);
+      Map<String, String> postMap = {
+        "uploadedImgUrl": imglink,
+        "uploaderName": " Aman",
+        "title": title,
+        "desc": description,
+        "Location": location,
+      };
+      helper.addData(postMap).then((value) {
+        print("upload sucessfull");
+      }); // upload image to firebase
     }
   }
 
@@ -39,120 +65,136 @@ class _AddPostState extends State<AddPost> {
         middle: Text('Add a Post'),
       ),
       child: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    selectImage();
-                  },
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                    child: imagePath == ""
-                        ? Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.add_a_photo_outlined,
-                                color: Colors.black26,
-                              ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                "Select a photo from the device",
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 10,
-                                ),
-                              ),
-                            ],
-                          )
-                        : Image.file(
-                            File(imagePath),
-                            fit: BoxFit.cover,
-                          ),
-                    height: 150,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[350],
-                      borderRadius: BorderRadius.circular(8),
-                    ),
+        child: isloading
+            ? Container(
+                child: Center(
+                  child: CupertinoActivityIndicator(
+                    radius: 20,
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+              )
+            : SingleChildScrollView(
+                child: Container(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "Title",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      CupertinoTextField(
-                        placeholder: "Enter a short title",
-                        onChanged: (value) {
-                          title = value;
+                      GestureDetector(
+                        onTap: () {
+                          selectImage();
                         },
-                      ),
-                      Text(
-                        "Description",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      CupertinoTextField(
-                        clearButtonMode: OverlayVisibilityMode.editing,
-                        placeholder: "Enter a short description",
-                        onChanged: (value) {
-                          description = value;
-                        },
-                      ),
-                      Text(
-                        "Location",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      CupertinoTextField(
-                        placeholder: "Enter a location",
-                      ),
-                      Center(
                         child: Container(
-                          margin: EdgeInsets.symmetric(vertical: 25),
-                          padding: EdgeInsets.all(10),
-                          child: CupertinoButton(
-                              color: Colors.blue,
-                              child: Text("Post"),
-                              onPressed: () {
-                                postBlog();
-                                print("clicked");
-                              }),
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20),
+                          child: imagePath == ""
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.add_a_photo_outlined,
+                                      color: Colors.black26,
+                                    ),
+                                    SizedBox(
+                                      height: 15,
+                                    ),
+                                    Text(
+                                      "Select a photo from the device",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : Image.file(
+                                  File(imagePath),
+                                  fit: BoxFit.cover,
+                                ),
+                          height: 150,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[350],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Title",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            CupertinoTextField(
+                              placeholder: "Enter a short title",
+                              onChanged: (value) {
+                                title = value;
+                              },
+                            ),
+                            Text(
+                              "Description",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            CupertinoTextField(
+                              clearButtonMode: OverlayVisibilityMode.editing,
+                              placeholder: "Enter a short description",
+                              onChanged: (value) {
+                                description = value;
+                              },
+                            ),
+                            Text(
+                              "Location",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            CupertinoTextField(
+                              placeholder: "Enter a location",
+                              onChanged: (value) {
+                                location = value;
+                              },
+                            ),
+                            Center(
+                              child: Container(
+                                margin: EdgeInsets.symmetric(vertical: 25),
+                                padding: EdgeInsets.all(10),
+                                child: CupertinoButton(
+                                    color: Colors.blue,
+                                    child: Text("Post"),
+                                    onPressed: () {
+                                      setState(() {
+                                        isloading = true;
+                                      });
+                                      print("clicked");
+                                      postBlog();
+                                    }),
+                              ),
+                            ),
+                          ],
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [],
                         ),
                       ),
                     ],
                   ),
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [],
-                  ),
                 ),
-              ],
-            ),
-          ),
-        ),
+              ),
       ),
     );
   }
