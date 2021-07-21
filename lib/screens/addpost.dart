95860571f056ec74294d310ee5f7c375d224f310
Aman Raj <amanpie9@gmail.com>
2021-07-21 15:02:05 +0530

    stable build before firebase crashing it

--- lib/screens/addpost.dart
+++ lib/screens/addpost.dart
@@ -1,12 +1,39 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class AddPost extends StatelessWidget {
+class AddPost extends StatefulWidget {
   const AddPost({Key? key}) : super(key: key);
+  @override
+  _AddPostState createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  String title = "", description = "", location = "";
+  String imagePath = "";
+  final picker = ImagePicker();
+  var pickedFile;
+  void selectImage() async {
+    pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        imagePath = pickedFile.path;
+      });
+    }
+  }
+
+  void postBlog() {
+    if (pickedFile == null) {
+      // add a pop up warning
+    } else {
+      // upload image to firebase
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    String title = "", description = "", location = "";
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('Add a Post'),
@@ -16,32 +43,42 @@ class AddPost extends StatelessWidget {
           child: Container(
             child: Column(
               children: [
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.add_a_photo_outlined,
-                        color: Colors.black26,
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        "Select a photo from the device",
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 10,
-                        ),
-                      )
-                    ],
-                  ),
-                  height: 150,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[350],
-                    borderRadius: BorderRadius.circular(8),
+                GestureDetector(
+                  onTap: () {
+                    selectImage();
+                  },
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: imagePath == ""
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add_a_photo_outlined,
+                                color: Colors.black26,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                "Select a photo from the device",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ],
+                          )
+                        : Image.file(
+                            File(imagePath),
+                            fit: BoxFit.cover,
+                          ),
+                    height: 150,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[350],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   ),
                 ),
                 Container(
@@ -98,6 +135,7 @@ class AddPost extends StatelessWidget {
                               color: Colors.blue,
                               child: Text("Post"),
                               onPressed: () {
+                                postBlog();
                                 print("clicked");
                               }),
                         ),
